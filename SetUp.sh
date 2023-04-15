c39daa1bf9b64d7149887fa89f6d9cfc3974e464
abtye <123047756+abtye@users.noreply.github.com>
2023-04-15 11:22:52 +0800

    Update SetUp.sh

--- SetUp.sh
+++ SetUp.sh
@@ -4,17 +4,17 @@
 a=$(node -v)
 if [ ${a:1:1} != 1 ];then
     echo -e "\033[31m你已经安装了Node，但是版本过低\033[0m"
-    read -p "[y/n]你是通过apt安装的吗？" isAptInstall
+    read -rp "[y/n]你是通过apt安装的吗？" isAptInstall
     if [ $isAptInstall == y ];then
-        read -p "[y/n]要删除Node并安装最新版本吗" isAptInstall
+        read -rp "[y/n]要删除Node并安装最新版本吗" isAptInstall
         if [ $isAptInstall == n ];then
-            read -p "按下回车键退出程序"
+            read -rp "按下回车键退出程序"
         else
             #判断是否有sudo权限
             sudo -n ls
             if [ $? != 0 ];then
                 echo -e "\033[31m你没有sudo权限\033[0m"
-                read -p "按下回车键退出"
+                read -rp "按下回车键退出"
             else
                 sudo apt purge node
             fi
@@ -24,20 +24,19 @@ fi
 # 检查是否安装git
 git --version
 if [ $? != 0 ];then
-    echo "你好像没有安装git"
-    read -p "按下回车键安装git"
+    echo -e "\033[34m你好像没有安装git\033[0m"
+    read -rp "按下回车键安装git"
     sudo apt install git
 fi
 #
 cd ~
-echo "！！！本脚本只适用于从未搭建过Node环境的Linux64位版本！！！"
-echo "2023-04-14版本"
-echo "-----------------------------"
-echo "[1]长期维护版v18.16.0"
-echo "[2]最新尝鲜版v19.8.1"
-echo "[3]17系列最新版v17.9.1"
-echo "[4]其他版本"
-echo "-----------------------------"
+echo -e "\033[34m！！！本脚本只适用于从未搭建过Node环境的Linux64位版本！！！\033[0m"
+echo -e "\033[34m2023-04-14版本\033[0m"
+echo -e "\033[34m[1]长期维护版v18.16.0\033[0m"
+echo -e "\033[34m[2]最新尝鲜版v19.8.1\033[0m"
+echo -e "\033[34m[3]17系列最新版v17.9.1\033[0m"
+echo -e "\033[34m[4]其他版本\033[0m"
+echo -e "\033[34m---------------------------------------\033[0m"
 #
 versionLinks=(
 "https://mirror.tuna.tsinghua.edu.cn/nodejs-release/v18.6.0/node-v18.6.0-linux-x64.tar.xz"
@@ -46,9 +45,9 @@ versionLinks=(
 )
 #
 downloadVersion() {
-    read -p "请选择下载版本（填序号）：" specificVersion
+    read -rp "请选择下载版本（填序号）：" specificVersion
     if [ $specificVersion == "4" ]; then
-    read -p "按下回车键打开下载网页并退出脚本"
+    read -rp "按下回车键打开下载网页并退出脚本"
     xdg-open https://mirror.tuna.tsinghua.edu.cn/nodejs-release/
     exit
     fi
@@ -59,8 +58,8 @@ downloadVersion() {
     fi
     #
     versionLink=${versionLinks[$specificVersion-1]}
-    wget $versionLink
-    tar -xvf node-*.tar.xz
+    wget -q $versionLink
+    tar -xvf node-*.tar.xz -q
     rm node-*.tar.xz
     mv node-* .node
     return 0
@@ -86,18 +85,43 @@ echo -e "\033[34m如果有输出版本号则成功\033[0m"
 cnpm -v
 echo -e "\033[34m---------------------------------------\033[0m"
 echo -e "\033[34m初始化项目\033[0m"
-
-wget https://i91.lanzoug.com/04150800110955609bb/2023/04/15/321630ad92f290f4106befef137b3427.7z?st=Jv3mO4rNUzsWzxwAReOQvQ&e=1681518834&b=U2APfwZ2AygHZQIu&fi=110955609&pid=112-47-64-212&up=2&mp=0&co=1
-if [ $? != 0 ];then
-    echo -e "\033[32m下载压缩包失败\033[0m"
-    echo -e "\033[32m[1]通过浏览器打开下载页面\033[0m"
-    echo -e "\033[32m[2]通过GitHub下载\033[0m"
-    echo -e "\033[32m[3]用npm初始化（极其不推荐，需魔法）\033[0m"
-    read -p "请选择解决方案"
+#
+wget -q "https://objects.githubusercontent.com/github-production-release-asset-2e65be/625408126/cbbd19f9-c38e-4ef6-97b7-79577d0c2ac3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230415%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230415T031845Z&X-Amz-Expires=300&X-Amz-Signature=d796ea11342a68545016af40df10c15f02e5bf76479813a7909505f6effe21e6&X-Amz-SignedHeaders=host&actor_id=123047756&key_id=0&repo_id=625408126&response-content-disposition=attachment%3B%20filename%3Dapp.tar.xz&response-content-type=application%2Foctet-stream"
+if [ $? != 0 ]; then
+    echo -e "\033[31m下载压缩包失败\033[0m"
+    echo -e "\033[32m[1]重新下载\033[0m"
+    echo -e "\033[32m[2]从兰奏云下载（不一定行）\033[0m"
+    echo -e "\033[32m[3]通过浏览器打开兰奏云下载页面（大概率行）\033[0m"
+    echo -e "\033[32m[4]通过浏览器打开GitHub下载页面（肯定行）\033[0m"
+    echo -e "\033[32m[5]用cnpm初始化（极其不推荐，神之bug，需要安装git）\033[0m"
+    read -rp "请选择解决方案: " choose
+#
+    case $choose in
+        1)
+            wget -cq "https://objects.githubusercontent.com/github-production-release-asset-2e65be/625408126/cbbd19f9-c38e-4ef6-97b7-79577d0c2ac3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20230415%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230415T031845Z&X-Amz-Expires=300&X-Amz-Signature=d796ea11342a68545016af40df10c15f02e5bf76479813a7909505f6effe21e6&X-Amz-SignedHeaders=host&actor_id=123047756&key_id=0&repo_id=625408126&response-content-disposition=attachment%3B%20filename%3Dapp.tar.xz&response-content-type=application%2Foctet-stream"
+            ;;
+        2)
+            wget https://developer.lanzoug.com/file/?BmBbZQ8+U2IDCgM7ADVVOVNsAztRbwFzVyAGLlw3UCsEIgY3DztQaAIxBA1Tbl04AGcANFE/ADIGOAAwVjxXZQY3Wz8PdFNhAyYDaABsVWRTPAMxUToBN1diBjhcJlAhBHQGbA9gUDQCZgRhUyhdbABuACpRPwA3Bi4AMlY+V2MGZ1s/D2pTNANkA2IAMlU3UzoDYlFtAWdXZAYzXDZQNARlBjIPZ1A1AmUENlMwXWoAOwBgUTsAZgY5AChWPVcsBiBbeQ8hU2IDJwM8ADBVaVM6AzdRPQExV2MGMVw4UHcEcAY4Dz9QYQIyBG9TNl1qAGIANFE7ADAGNgA0VjNXbgYoWyIPdFNhAzkDIgBpVWRTLwNzUX4BdFdtBjFcNlBpBDUGZA9gUDICZQRjUz5degAvAGxRfgA/BjAAN1Y4V3gGMls7D3xTMQNhA3sAZlVlUzs=
+            ;;
+        3)
+            echo -e "\033[32m密码：1111\033[0m"
+            read -rp "按下回车键跳转"
+            xdg-open https://wwlf.lanzoue.com/iTgyQ0t1d4kd
+            ;;
+        4)
+            xdg-open https://github.com/abtye/SetNodeEnvironment/tags
+            ;;
+        5)
+            cnpm init electron-app@latest app
+            ;;
+        *)
+            echo -e "\033[31m错误的输入\033[0m"
+            ;;
+    esac
 fi
-echo "------------------------"
+#
 echo -e "\033[32m项目初始化成功，请将代码放置于./app/src下\033[0m"
-echo -e "\033[31m！！！初始页面必须是index.html！！！[0m"
-echo -e "\033[31m！！！index.js必须保留！！！[0m"
-read -p "脚本执行完成，按回车键退出"
+echo -e "\033[31m！！！初始页面必须是index.html！！！\033[0m"
+echo -e "\033[31m！！！index.js必须保留！！！\033[0m"
+read -rp "脚本执行完成，按回车键退出"
 exit
